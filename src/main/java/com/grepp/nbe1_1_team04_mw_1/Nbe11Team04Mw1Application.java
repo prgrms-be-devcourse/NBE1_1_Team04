67370de47f9a38e91ad49cdaa74203059d6c5c1a
HMWG <nurung22222@gmail.com>
2024-09-06 14:17:10 +0900

    init : first init

---
+++ src/main/java/com/grepp/nbe1_1_team04_mw_1/Nbe11Team04Mw1Application.java
@@ -0,0 +1,13 @@
+package com.grepp.nbe1_1_team04_mw_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nbe11Team04Mw1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nbe11Team04Mw1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/grepp/nbe1_1_team04_mw_1/Nbe11Team04Mw1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grepp.nbe1_1_team04_mw_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nbe11Team04Mw1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
