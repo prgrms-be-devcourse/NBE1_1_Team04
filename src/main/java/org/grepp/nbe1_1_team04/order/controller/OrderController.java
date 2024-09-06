db02c187ce1140b5f9f68d8f4de341c82922f09a
icecoff22 <icecoff2222@gmail.com>
2024-09-06 17:26:50 +0900

    fix : url endpoint 명세서에 맞게 수정

--- src/main/java/org/grepp/nbe1_1_team04/order/controller/OrderController.java
+++ src/main/java/org/grepp/nbe1_1_team04/order/controller/OrderController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/order")
+@RequestMapping("/orders")
 public class OrderController {
 
     private OrderService orderService;
