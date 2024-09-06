c0d024c8ef0c0bbd3c10b58a524ce42086987ec5
icecoff22 <icecoff2222@gmail.com>
2024-09-06 16:46:04 +0900

    feat : entity 속 enum 의 string화, getter 설정

--- src/main/java/org/grepp/nbe1_1_team04/order/entity/OrderEntity.java
+++ src/main/java/org/grepp/nbe1_1_team04/order/entity/OrderEntity.java
@@ -1,6 +1,8 @@
 package org.grepp.nbe1_1_team04.order.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
 import org.grepp.nbe1_1_team04.global.entity.BaseTimeEntity;
 
@@ -11,5 +13,26 @@ public class OrderEntity extends BaseTimeEntity {
     private String email;
     private String address;
     private String postcode;
+    @Enumerated(EnumType.STRING)
     private OrderStatus orderStatus;
+
+    public Byte[] getOrderId() {
+        return orderId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPostcode() {
+        return postcode;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return orderStatus;
+    }
 }
--- src/main/java/org/grepp/nbe1_1_team04/product/entity/ProductEntity.java
+++ src/main/java/org/grepp/nbe1_1_team04/product/entity/ProductEntity.java
@@ -1,6 +1,8 @@
 package org.grepp.nbe1_1_team04.product.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
 import org.grepp.nbe1_1_team04.global.entity.BaseTimeEntity;
 import org.grepp.nbe1_1_team04.order.entity.OrderStatus;
@@ -12,6 +14,28 @@ public class ProductEntity extends BaseTimeEntity {
     private Byte[] productId;
     private String productName;
     private String description;
+
+    @Enumerated(EnumType.STRING)
     private ProductCategory category;
     private Long price;
+
+    public Byte[] getProductId() {
+        return productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public ProductCategory getCategory() {
+        return category;
+    }
+
+    public Long getPrice() {
+        return price;
+    }
 }
