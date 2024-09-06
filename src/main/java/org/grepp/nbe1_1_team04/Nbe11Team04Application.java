8607e30d02373e94ae2b7c40a4bbed40c07ec250
icecoff22 <icecoff2222@gmail.com>
2024-09-06 16:17:54 +0900

    init : 프로젝트 생성

---
+++ src/main/java/org/grepp/nbe1_1_team04/Nbe11Team04Application.java
@@ -0,0 +1,13 @@
+package org.grepp.nbe1_1_team04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nbe11Team04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nbe11Team04Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/grepp/nbe1_1_team04/Nbe11Team04ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.grepp.nbe1_1_team04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nbe11Team04ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
