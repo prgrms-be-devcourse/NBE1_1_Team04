438e74a99e319180435e9cce8a524dc51eada9c9
jhk01007 <jhk001007@naver.com>
2024-09-06 14:16:40 +0900

    init

---
+++ src/test/java/org/example/coffee/CoffeeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.coffee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
