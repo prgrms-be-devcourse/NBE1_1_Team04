c8ed7dcb1e91a703defb708af9485ad1b007fd5b
jhk01007 <jhk001007@naver.com>
2024-09-09 00:30:16 +0900

    fix, feat: 기본키 타입 변경, 이메일로 주문 찾아오는 메서드 추가
    fix: UUID -> byte[]로 기본키 타입 변경
    feat: findByEmail: 이메일로 Order 찾아옴

--- src/main/java/org/example/coffee/repository/OrderRepository.java
+++ src/main/java/org/example/coffee/repository/OrderRepository.java
@@ -4,8 +4,10 @@ import org.example.coffee.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
-public interface OrderRepository extends JpaRepository<Order, UUID> {
+public interface OrderRepository extends JpaRepository<Order, byte[]> {
+    List<Order> findByEmail(String email);
 }
