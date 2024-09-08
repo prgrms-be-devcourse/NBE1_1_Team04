1be2b9f3c73dae3dd1f23d6908a3ed553ed6a1aa
jhk01007 <jhk001007@naver.com>
2024-09-09 00:25:11 +0900

    feat: Order 컨트롤러 구현 완료

---
+++ src/main/java/org/example/coffee/api/OrderApi.java
@@ -0,0 +1,63 @@
+package org.example.coffee.api;
+
+import org.example.coffee.dto.OrderRequest;
+import org.example.coffee.dto.OrderResponse;
+import org.example.coffee.service.OrderService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping ("/orders")
+public class OrderApi {
+
+    private final OrderService orderService;
+
+    public OrderApi(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    /**
+     * 전체 주문 조히
+     * 테스트용
+     */
+    @GetMapping
+    public List<OrderResponse> getProduct() {
+        return orderService.findAllOrders();
+    }
+
+
+    /**
+     * 특정 주문 조회
+     * 이메일로 검증
+     */
+    @GetMapping("{id}")
+    public OrderResponse getProduct(@PathVariable("id") String orderId, @RequestParam("email") String email) {
+        return orderService.findOrder(orderId, email);
+    }
+
+    /**
+     * 주문 추가
+     */
+    @PostMapping
+    public OrderResponse addOrder(@RequestBody OrderRequest orderRequest) {
+        return orderService.addOrder(orderRequest);
+    }
+
+    /**
+     * 주문 수정
+     */
+    @PutMapping("{id}")
+    public OrderResponse editOrder(@PathVariable("id") String orderId, @RequestBody OrderRequest orderRequest) {
+        return orderService.editOrder(orderId, orderRequest);
+    }
+
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> deleteOrder(@PathVariable("id") String orderId) {
+        orderService.deleteOrder(orderId);
+
+        return ResponseEntity.ok("ORDER_DELETED");
+    }
+}
