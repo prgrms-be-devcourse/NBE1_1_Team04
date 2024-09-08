a6dae61d01186348520cad8043dd7578c570ae88
jhk01007 <jhk001007@naver.com>
2024-09-09 00:20:25 +0900

    feat: ProductApi - Product 컨트롤러 구현

---
+++ src/main/java/org/example/coffee/api/ProductApi.java
@@ -0,0 +1,64 @@
+package org.example.coffee.api;
+
+import org.example.coffee.dto.ProductRequest;
+import org.example.coffee.dto.ProductResponse;
+import org.example.coffee.service.ProductService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class ProductApi {
+
+    private final ProductService productService;
+
+    public ProductApi(ProductService productService) {
+        this.productService = productService;
+    }
+
+    /**
+     * 상품 전체 조회
+     */
+    @GetMapping
+    public List<ProductResponse> getProductList() {
+        return productService.findAllProducts();
+    }
+
+    /**
+     * 특정 상품 조회
+     */
+    @GetMapping("/{id}")
+    public ProductResponse getProduct(@PathVariable("id") String productId) {
+        return productService.findProduct(productId);
+    }
+
+    /**
+     * 상품 등록
+     */
+    @PostMapping
+    public ProductResponse addProduct(@RequestBody ProductRequest productRequest) {
+        return productService.addProduct(productRequest);
+    }
+
+    /**
+     * 상품 수정
+     */
+    @PutMapping("/{id}")
+    public ProductResponse editProduct(@PathVariable("id") String productID, @RequestBody ProductRequest productRequest) {
+        return productService.editProduct(productID, productRequest);
+    }
+
+
+    /**
+     * 상품 삭제
+     */
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteProduct(@PathVariable("id") String productId) {
+        productService.deleteProduct(productId);
+
+        return ResponseEntity.ok("PRODUCT_DELETED");
+    }
+
+}
