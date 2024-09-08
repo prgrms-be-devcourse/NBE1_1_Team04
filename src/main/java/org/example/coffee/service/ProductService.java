924c911f7c118557eb123d747ca882b6235b1895
jhk01007 <jhk001007@naver.com>
2024-09-09 00:19:59 +0900

    feat: ProductService 서비스 계층 구현

---
+++ src/main/java/org/example/coffee/service/ProductService.java
@@ -0,0 +1,65 @@
+package org.example.coffee.service;
+
+import org.example.coffee.dto.ProductRequest;
+import org.example.coffee.dto.ProductResponse;
+import org.example.coffee.entity.Product;
+import org.example.coffee.exception.ProductNotFoundException;
+import org.example.coffee.mapper.ProductMapper;
+import org.example.coffee.repository.ProductRepository;
+import org.example.coffee.util.UUIDUtil;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+
+    public List<ProductResponse> findAllProducts() {
+        return productRepository.findAll()
+                .stream()
+                .map(ProductMapper::toResponse)
+                .collect(Collectors.toList());
+    }
+
+    public ProductResponse findProduct(String productId) {
+        Product product = productRepository.findById(UUIDUtil.hexToBytes(productId)).orElseThrow(() -> new ProductNotFoundException("없는 상품입니다."));
+        return ProductMapper.toResponse(product);
+    }
+
+    public ProductResponse addProduct(ProductRequest productRequest) {
+        Product saved = productRepository.save(ProductMapper.toProduct(productRequest));
+        return ProductMapper.toResponse(saved);
+    }
+
+    @Transactional
+    public ProductResponse editProduct(String productId, ProductRequest productRequest) {
+        Product product = productRepository.findById(UUIDUtil.hexToBytes(productId))
+                .orElseThrow(() -> new ProductNotFoundException("없는 상품입니다."));
+
+        if(productRequest.getProductName() != null) product.setProductName(productRequest.getProductName());
+        if(productRequest.getCategory() != null) product.setCategory(productRequest.getCategory());
+        if(productRequest.getPrice() != null) product.setPrice(productRequest.getPrice());
+        if(productRequest.getDescription() != null) product.setDescription(productRequest.getDescription());
+
+        // 수정시간 업데이트
+        product.setUpdatedAt(LocalDateTime.now());
+
+        return ProductMapper.toResponse(product);
+    }
+
+    public void deleteProduct(String productId) {
+        productRepository.deleteById(UUIDUtil.hexToBytes(productId));
+    }
+
+}
+
