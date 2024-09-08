ccdf1865c558d8457a8a79b6bed64c8850bbf256
icecoff22 <icecoff2222@gmail.com>
2024-09-08 11:54:28 +0900

    feat : product api 생성

--- src/main/java/org/grepp/nbe1_1_team04/product/controller/ProductController.java
+++ src/main/java/org/grepp/nbe1_1_team04/product/controller/ProductController.java
@@ -1,8 +1,13 @@
 package org.grepp.nbe1_1_team04.product.controller;
 
+import org.grepp.nbe1_1_team04.product.dto.ProductRequest;
+import org.grepp.nbe1_1_team04.product.dto.ProductResponse;
 import org.grepp.nbe1_1_team04.product.service.ProductService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/products")
@@ -13,6 +18,38 @@ public class ProductController {
     public ProductController(ProductService productService) {
         this.productService = productService;
     }
+
+    @PostMapping
+    public ResponseEntity<HttpMethod> createProduct(@RequestBody ProductRequest productRequest) {
+        productService.registerProduct(productRequest);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProductResponse>> getProducts() {
+        return ResponseEntity.ok().body(productService.getProducts());
+    }
+
+    @GetMapping("/{productId}")
+    public ResponseEntity<ProductResponse> getProduct(@PathVariable long productId) {
+        return ResponseEntity.ok().body(productService.getProduct());
+    }
+
+    @PutMapping("/{productId}")
+    public ResponseEntity<HttpMethod> updateProduct(@RequestBody ProductRequest productRequest, @PathVariable long productId) {
+        productService.updateProduct(productRequest, productId);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{productId}")
+    public ResponseEntity<HttpMethod> deleteProduct(@PathVariable long productId) {
+        productService.deleteProduct(productId);
+        return ResponseEntity.ok().build();
+    }
+
+
+
+
 
 
 }
--- src/main/java/org/grepp/nbe1_1_team04/product/service/ProductService.java
+++ src/main/java/org/grepp/nbe1_1_team04/product/service/ProductService.java
@@ -1,7 +1,11 @@
 package org.grepp.nbe1_1_team04.product.service;
 
+import org.grepp.nbe1_1_team04.product.dto.ProductRequest;
+import org.grepp.nbe1_1_team04.product.dto.ProductResponse;
 import org.grepp.nbe1_1_team04.product.repository.ProductRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProductService {
@@ -9,5 +13,25 @@ public class ProductService {
 
     public ProductService(ProductRepository productRepository) {
         this.productRepository = productRepository;
+    }
+
+    public void registerProduct(ProductRequest productRequest) {
+
+    }
+
+    public List<ProductResponse> getProducts() {
+        return  null;
+    }
+
+    public ProductResponse getProduct() {
+        return null;
+    }
+
+    public void updateProduct(ProductRequest productRequest, long productId) {
+
+    }
+
+    public void deleteProduct(long productId) {
+
     }
 }
