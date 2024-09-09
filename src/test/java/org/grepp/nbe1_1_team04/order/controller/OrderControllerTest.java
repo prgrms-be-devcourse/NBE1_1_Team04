3c50c87595cc5ee1eaab94ae8990432056d34a18
icecoff22 <icecoff2222@gmail.com>
2024-09-09 11:04:54 +0900

    test : 주문 컨트롤러, 서비스 테스트코드 작성

--- src/test/java/org/grepp/nbe1_1_team04/order/controller/OrderControllerTest.java
+++ src/test/java/org/grepp/nbe1_1_team04/order/controller/OrderControllerTest.java
@@ -1,2 +1,108 @@
-package org.grepp.nbe1_1_team04.order.controller;public class OrderControllerTest {
+package org.grepp.nbe1_1_team04.order.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.grepp.nbe1_1_team04.order.dto.OrderItemInfo;
+import org.grepp.nbe1_1_team04.order.dto.OrderRequest;
+import org.grepp.nbe1_1_team04.order.service.OrderService;
+import org.grepp.nbe1_1_team04.product.service.ProductService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = OrderController.class)
+public class OrderControllerTest {
+
+    @MockBean
+    private OrderService orderService;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Test
+    @DisplayName("주문 생성 api 테스트")
+    void createOrderTest() throws Exception {
+        //given
+        OrderRequest orderRequest = new OrderRequest("test@test.com", "주소1", "우편번호1", new ArrayList<>());
+        orderRequest.getOrderItems().add(new OrderItemInfo("1234".getBytes(), 50));
+        orderRequest.getOrderItems().add(new OrderItemInfo("5678".getBytes(), 100));
+        String orderRequestBody = objectMapper.writeValueAsString(orderRequest);
+
+        //when
+        MockHttpServletRequestBuilder requestBuilder = post("/orders")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(orderRequestBody);
+        ResultActions resultActions = mockMvc.perform(requestBuilder);
+
+        //then
+        resultActions.andExpect(status().isCreated());
+    }
+
+    @Test
+    @DisplayName("주문 리스트 조회 api 테스트")
+    void getProductsTest() throws Exception {
+        //when
+        MockHttpServletRequestBuilder requestBuilder = get("/orders")
+                .contentType(MediaType.APPLICATION_JSON);
+        ResultActions resultActions = mockMvc.perform(requestBuilder);
+
+        //then
+        resultActions.andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("특정 주문 조회 api 테스트")
+    void getProductTest() throws Exception {
+        //when
+        MockHttpServletRequestBuilder requestBuilder = get("/orders/1")
+                .contentType(MediaType.APPLICATION_JSON);
+        ResultActions resultActions = mockMvc.perform(requestBuilder);
+
+        //then
+        resultActions.andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("특정 주문 수정 api 테스트")
+    void updateProductTest() throws Exception {
+        //given
+        OrderRequest orderRequest = new OrderRequest("test@test.com", "주소1", "우편번호1", new ArrayList<>());
+        orderRequest.getOrderItems().add(new OrderItemInfo("1234".getBytes(), 50));
+        orderRequest.getOrderItems().add(new OrderItemInfo("5678".getBytes(), 100));
+        String orderRequestBody = objectMapper.writeValueAsString(orderRequest);
+
+        //when
+        MockHttpServletRequestBuilder requestBuilder = put("/orders/1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(orderRequestBody);
+        ResultActions resultActions = mockMvc.perform(requestBuilder);
+
+        //then
+        resultActions.andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("특정 주문 삭제 api 테스트")
+    void deleteProductTest() throws Exception {
+        //when
+        MockHttpServletRequestBuilder requestBuilder = delete("/orders/1")
+                .contentType(MediaType.APPLICATION_JSON);
+        ResultActions resultActions = mockMvc.perform(requestBuilder);
+
+        //then
+        resultActions.andExpect(status().isOk());
+    }
 }
--- src/test/java/org/grepp/nbe1_1_team04/order/service/OrderServiceTest.java
+++ src/test/java/org/grepp/nbe1_1_team04/order/service/OrderServiceTest.java
@@ -1,2 +1,136 @@
-package org.grepp.nbe1_1_team04.order.service;public class OrderServiceTest {
+package org.grepp.nbe1_1_team04.order.service;
+
+import org.grepp.nbe1_1_team04.order.dto.OrderItemInfo;
+import org.grepp.nbe1_1_team04.order.dto.OrderRequest;
+import org.grepp.nbe1_1_team04.order.dto.OrderResponse;
+import org.grepp.nbe1_1_team04.order.entity.OrderEntity;
+import org.grepp.nbe1_1_team04.order.entity.OrderItemsEntity;
+import org.grepp.nbe1_1_team04.order.entity.OrderStatus;
+import org.grepp.nbe1_1_team04.order.repository.OrderItemsRepository;
+import org.grepp.nbe1_1_team04.order.repository.OrderRepository;
+import org.grepp.nbe1_1_team04.product.entity.ProductCategory;
+import org.grepp.nbe1_1_team04.product.entity.ProductEntity;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class OrderServiceTest {
+    @InjectMocks
+    private OrderService orderService;
+
+    @Mock
+    private OrderRepository orderRepository;
+
+    @Mock
+    private OrderItemsRepository orderItemsRepository;
+
+    @Test
+    @DisplayName("주문 추가 테스트")
+    void registerOrder() {
+        //given
+        OrderRequest orderRequest = new OrderRequest("test@test.com", "주소1", "우편번호1", new ArrayList<>());
+        orderRequest.getOrderItems().add(new OrderItemInfo("1234".getBytes(), 50));
+        orderRequest.getOrderItems().add(new OrderItemInfo("5678".getBytes(), 100));
+
+        //when
+        orderService.createOrder(orderRequest);
+
+        //then
+        verify(orderRepository, times(1)).save(any(OrderEntity.class));
+        verify(orderItemsRepository, times(orderRequest.getOrderItems().size())).save(any(OrderItemsEntity.class));
+    }
+
+    @Test
+    @DisplayName("주문 리스트 조회 테스트")
+    void getOrdersTest() {
+        //given
+        OrderEntity newOrder1 = new OrderEntity("1234".getBytes(), "test@test.com", "주소1", "우편번호1", OrderStatus.valueOf("주문완료"));
+        OrderEntity newOrder2 = new OrderEntity("5678".getBytes(), "test@test.com", "주소1", "우편번호1", OrderStatus.valueOf("배송중"));
+        ProductEntity newProduct1 = new ProductEntity("1235".getBytes(), "이름1", "설명1", ProductCategory.COFFEE, 5000L);
+        ProductEntity newProduct2 = new ProductEntity("1237".getBytes(), "이름2", "설명2", ProductCategory.COFFEE, 3000L);
+
+        List<OrderEntity> orderEntities = List.of(newOrder1, newOrder2);
+        List<OrderItemsEntity> orderItemEntities = List.of(
+                new OrderItemsEntity(5000, 3, ProductCategory.COFFEE, newOrder1, newProduct1),
+                new OrderItemsEntity(3000, 5, ProductCategory.COFFEE, newOrder1, newProduct2)
+        );
+
+        when(orderRepository.findAll()).thenReturn(orderEntities);
+        when(orderItemsRepository.findByOrder(newOrder1)).thenReturn(orderItemEntities);
+
+        //when
+        List<OrderResponse> orderResponses = orderService.getOrders();
+
+        //then
+        assertThat(orderResponses.size()).isEqualTo(2);
+        assertThat(orderResponses.get(0).getProducts().size()).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("특정 주문 조회 테스트")
+    void getProductTest() {
+        //given
+        byte[] orderId = "1234".getBytes();
+
+        Optional<OrderEntity> orderEntity = Optional.of(new OrderEntity(orderId, "test@test.com", "주소1", "우편번호1",OrderStatus.valueOf("주문완료")));
+        ProductEntity newProduct1 = new ProductEntity("1235".getBytes(), "이름1", "설명1", ProductCategory.COFFEE, 5000L);
+        ProductEntity newProduct2 = new ProductEntity("1237".getBytes(), "이름2", "설명2", ProductCategory.COFFEE, 3000L);
+        List<OrderItemsEntity> orderItemEntities = List.of(
+                new OrderItemsEntity(5000, 3, ProductCategory.COFFEE, orderEntity.get(), newProduct1),
+                new OrderItemsEntity(3000, 5, ProductCategory.COFFEE, orderEntity.get(), newProduct2)
+        );
+        when(orderRepository.findById(orderId)).thenReturn(orderEntity);
+        when(orderItemsRepository.findByOrder(orderEntity.get())).thenReturn(orderItemEntities);
+
+        //when
+        OrderResponse orderResponse = orderService.getOrder(orderId);
+
+        //then
+        assertThat(orderResponse).isNotNull();
+        assertThat(orderResponse.getProducts().size()).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("주문 업데이트 테스트")
+    void updateProductTest() {
+        //given
+        byte[] orderId = "1234".getBytes();
+        OrderRequest orderRequest = new OrderRequest("test@test.com", "주소1", "우편번호1", new ArrayList<>());
+        orderRequest.getOrderItems().add(new OrderItemInfo("1234".getBytes(), 50));
+        orderRequest.getOrderItems().add(new OrderItemInfo("5678".getBytes(), 100));
+
+        //when
+        orderService.updateOrder(orderRequest, orderId);
+
+        //then
+        verify(orderRepository, times(1)).save(any(OrderEntity.class));
+        verify(orderItemsRepository, times(orderRequest.getOrderItems().size())).save(any(OrderItemsEntity.class));
+    }
+
+    @Test
+    @DisplayName("상품 삭제 테스트")
+    void deleteProductTest() {
+        //given
+        byte[] orderId = "1234".getBytes();
+
+        //when
+        orderService.deleteOrder(orderId);
+
+        //then
+        verify(orderRepository, times(1)).deleteById(orderId);
+        verify(orderItemsRepository).deleteByOrder(any(OrderEntity.class));
+
+    }
 }
