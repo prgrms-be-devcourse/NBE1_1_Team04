a7370d2a473889642227a5c80d7862f75a2b8890
jhk01007 <jhk001007@naver.com>
2024-09-09 14:09:23 +0900

    fix: Order 엔티티를 OrderRespons로 전환할 때 발생하는 java.lang.IllegalStateException: Duplicate key 해결
    - 한 주문에서 같은 상품을 여러번 나누어서 주문하는 경우가 있기 때문에 상품 중복 키가 발생할 수 있음.
      toMap()메서드는 중복키가 발생하면 예외를 발생시키는데
      그 때, 수량을 합치도록 했음

--- src/main/java/org/example/coffee/order/OrderMapper.java
+++ src/main/java/org/example/coffee/order/OrderMapper.java
@@ -31,14 +31,19 @@ public class OrderMapper {
     }
 
     public static OrderResponse toResponse(Order order) {
-
         return new OrderResponse(
                 UUIDUtil.bytesToHex(order.getOrderId()),
                 order.getEmail(),
                 order.getAddress(),
                 order.getPostcode(),
                 order.getOrderItemList().stream()
-                        .collect(Collectors.toMap(orderItem -> ProductMapper.toResponse(orderItem.getProduct()), OrderItem::getQuantity)),
+                        .collect(Collectors.toMap(
+                                orderItem -> ProductMapper.toResponse(orderItem.getProduct()),
+                                OrderItem::getQuantity,
+                                Integer::sum // 한 주문에서 같은 상품을 여러번 나누어서 주문하는 경우가 있기 때문에 상품 중복 키가 발생할 수 있음.
+                                            // toMap()메서드는 중복키가 발생하면 예외를 발생시키는데
+                                            // 그 때, 수량을 합치도록 했음
+                        )),
                 order.getOrderItemList().stream()
                         .mapToLong(orderItem -> orderItem.getPrice() * orderItem.getQuantity())
                         .sum(),
