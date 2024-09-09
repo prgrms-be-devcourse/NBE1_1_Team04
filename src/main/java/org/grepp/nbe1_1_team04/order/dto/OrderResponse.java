c0bed4cbc108ba5efea242ae9db4965a929fcd9b
icecoff22 <icecoff2222@gmail.com>
2024-09-09 11:29:57 +0900

    refactor : 주문 응답 dto 주문번호 필드 추가

--- src/main/java/org/grepp/nbe1_1_team04/order/dto/OrderResponse.java
+++ src/main/java/org/grepp/nbe1_1_team04/order/dto/OrderResponse.java
@@ -3,16 +3,22 @@ package org.grepp.nbe1_1_team04.order.dto;
 import java.util.List;
 
 public class OrderResponse {
+    private byte[] orderId;
     private String email;
     private String address;
     private String postCode;
     private List<OrderItemInfo> orderItems;
 
-    public OrderResponse(String email, String address, String postCode, List<OrderItemInfo> orderItems) {
+    public OrderResponse(byte[] orderId, String email, String address, String postCode, List<OrderItemInfo> orderItems) {
+        this.orderId = orderId;
         this.email = email;
         this.address = address;
         this.postCode = postCode;
         this.orderItems = orderItems;
+    }
+
+    public byte[] getOrderId() {
+        return orderId;
     }
 
     public String getEmail() {
