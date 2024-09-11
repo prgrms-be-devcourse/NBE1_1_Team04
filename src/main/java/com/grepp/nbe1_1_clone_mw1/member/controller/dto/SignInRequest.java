1ff20912d9c163d53c597ca58e9350e6643384ee
icecoff22 <icecoff2222@gmail.com>
2024-09-11 11:11:10 +0900

    feat : 회원가입, 로그인 요청 dto 작성

--- src/main/java/com/grepp/nbe1_1_clone_mw1/member/controller/dto/SignInRequest.java
+++ src/main/java/com/grepp/nbe1_1_clone_mw1/member/controller/dto/SignInRequest.java
@@ -1,2 +1,18 @@
-package com.grepp.nbe1_1_clone_mw1.member.controller.dto;public class SignInRequest {
+package com.grepp.nbe1_1_clone_mw1.member.controller.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class SignInRequest {
+
+    @NotBlank(message = "이메일은 필수입니다.")
+    private String email;
+
+    @NotBlank(message = "비밀번호를 입력해주세요.")
+    private String password;
 }
