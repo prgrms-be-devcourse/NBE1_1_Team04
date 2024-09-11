e6d42c482dcb58b10062f9b6db14b3054682cc48
icecoff22 <icecoff2222@gmail.com>
2024-09-11 15:14:00 +0900

    chore : 스프링 시큐리티 설정
    - 상품 권한 admin만, 그 외 허용
    - 세션으로 구현, csrf 보호 설정
    - csrf 토큰은 서버에서 내려주므로 최초 api는 2번 날려야 정상 응답이 감

--- src/main/java/com/grepp/nbe1_1_clone_mw1/global/configuration/SecurityConfiguration.java
+++ src/main/java/com/grepp/nbe1_1_clone_mw1/global/configuration/SecurityConfiguration.java
@@ -1,2 +1,46 @@
-package com.grepp.nbe1_1_clone_mw1.global.configuration;public class SecurityConfig {
+package com.grepp.nbe1_1_clone_mw1.global.configuration;
+
+import com.sun.net.httpserver.HttpsConfigurator;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+
+    private final CsrfConfiguration csrfConfiguration;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+//                .csrf(AbstractHttpConfigurer::disable);
+                .csrf(httpSecurityCsrfConfigurer -> {
+                    httpSecurityCsrfConfigurer.csrfTokenRequestHandler(csrfConfiguration.requestAttributeHandler());
+                    httpSecurityCsrfConfigurer.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+                });
+
+        http
+                .authorizeHttpRequests((authorize) -> {
+                    authorize
+                            .requestMatchers("/api/v1/products/**").hasRole("ADMIN")
+                            .anyRequest().permitAll();
+                });
+
+        return http.build();
+    }
 }
