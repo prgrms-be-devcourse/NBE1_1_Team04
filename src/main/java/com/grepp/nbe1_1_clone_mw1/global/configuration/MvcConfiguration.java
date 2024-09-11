bee302fd70f05948859f2b3bdccbcf93ab109980
icecoff22 <icecoff2222@gmail.com>
2024-09-12 03:22:05 +0900

    feat : @LoginUser 커스텀 어노테이션 추가
    - 유저 정보를 불러와야할 때 필요하여 추가

--- src/main/java/com/grepp/nbe1_1_clone_mw1/global/configuration/MvcConfiguration.java
+++ src/main/java/com/grepp/nbe1_1_clone_mw1/global/configuration/MvcConfiguration.java
@@ -1,13 +1,22 @@
 package com.grepp.nbe1_1_clone_mw1.global.configuration;
 
+import com.grepp.nbe1_1_clone_mw1.global.annotation.LoginUserArgumentResolver;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 public class MvcConfiguration implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/api/**").allowedOrigins("*");
+  }
+
+  @Override
+  public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+    resolvers.add(new LoginUserArgumentResolver());
   }
 }
