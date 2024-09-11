c39803505983dbe1254d6a4e7089834b69c4775e
HMWG <nurung22222@gmail.com>
2024-09-11 11:22:23 +0900

    feat : 상품 수정 및 삭제 컨트롤러 구현
    - ProductController 삭제 및 수정 구현
    - 타임리프 템플릿 product-list 수정 및 update-product 페이지 추가

--- src/main/java/com/grepp/nbe1_1_clone_mw1/product/controller/ProductController.java
+++ src/main/java/com/grepp/nbe1_1_clone_mw1/product/controller/ProductController.java
@@ -2,11 +2,13 @@ package com.grepp.nbe1_1_clone_mw1.product.controller;
 
 
 import com.grepp.nbe1_1_clone_mw1.product.controller.dto.CreateProductRequest;
+import com.grepp.nbe1_1_clone_mw1.product.controller.dto.UpdateProductRequest;
 import com.grepp.nbe1_1_clone_mw1.product.service.ProductService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ProductController {
@@ -36,6 +38,18 @@ public class ProductController {
       createProductRequest.category(),
       createProductRequest.price(),
       createProductRequest.description());
+    return "redirect:/products";
+  }
+
+  @GetMapping("update-product")
+  public String updateProductPage(@RequestParam("productId") String productId, Model model) {
+    model.addAttribute("productId", productId);
+    return "update-product";
+  }
+
+  @PostMapping("/products/update")
+  public String updateProduct(@RequestParam("productId") String productId, UpdateProductRequest updateProductRequest) {
+    productService.updateProduct(productId, updateProductRequest);
     return "redirect:/products";
   }
 
