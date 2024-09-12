c499bea5abf2bbdb866e63c413200da1d832f061
HMWG <nurung22222@gmail.com>
2024-09-12 10:16:47 +0900

    fix : 로그인 된 회원 정보로 리뷰 수정, 삭제 가능 로직 변경

--- src/main/java/com/grepp/nbe1_1_clone_mw1/review/controller/api/ReviewRestController.java
+++ src/main/java/com/grepp/nbe1_1_clone_mw1/review/controller/api/ReviewRestController.java
@@ -1,5 +1,7 @@
 package com.grepp.nbe1_1_clone_mw1.review.controller.api;
 
+import com.grepp.nbe1_1_clone_mw1.auth.model.CustomUserDetail;
+import com.grepp.nbe1_1_clone_mw1.global.annotation.LoginUser;
 import com.grepp.nbe1_1_clone_mw1.review.controller.dto.ReviewRequest;
 import com.grepp.nbe1_1_clone_mw1.review.controller.dto.ReviewResponse;
 import com.grepp.nbe1_1_clone_mw1.review.service.ReviewService;
@@ -16,8 +18,8 @@ public class ReviewRestController {
     private final ReviewService reviewService;
 
     @GetMapping("/review")
-    public ResponseEntity<List<ReviewResponse>> getReviews(byte[] userId) {
-        return reviewService.getReviewsByUser(userId);
+    public ResponseEntity<List<ReviewResponse>> getReviews(@LoginUser CustomUserDetail userDetail) {
+        return reviewService.getReviewsByUser(userDetail);
     }
 
     @GetMapping("/review/{productId}")
@@ -26,17 +28,17 @@ public class ReviewRestController {
     }
 
     @PostMapping("/review/{productId}")
-    public ResponseEntity<String> addReview(@PathVariable("productId") String productId, @RequestBody ReviewRequest reviewRequest, byte[] userId) {
-        return reviewService.createReview(productId, userId, reviewRequest);
+    public ResponseEntity<String> addReview(@PathVariable("productId") String productId, @RequestBody ReviewRequest reviewRequest, @LoginUser CustomUserDetail userDetail) {
+        return reviewService.createReview(productId, userDetail, reviewRequest);
     }
 
     @PutMapping("/review/{reviewId}")
-    public ResponseEntity<String> updateReview(@PathVariable("reviewId") Long reviewId, @RequestBody ReviewRequest reviewRequest, byte[] userId) {
-        return reviewService.updateReview(reviewId, userId, reviewRequest);
+    public ResponseEntity<String> updateReview(@PathVariable("reviewId") Long reviewId, @RequestBody ReviewRequest reviewRequest, @LoginUser CustomUserDetail userDetail) {
+        return reviewService.updateReview(reviewId, userDetail, reviewRequest);
     }
 
     @DeleteMapping("/review/{reviewId}")
-    public ResponseEntity<String> deleteReview(@PathVariable("reviewId") Long reviewId, byte[] userId) {
-        return reviewService.deleteReview(reviewId, userId);
+    public ResponseEntity<String> deleteReview(@PathVariable("reviewId") Long reviewId, @LoginUser CustomUserDetail userDetail) {
+        return reviewService.deleteReview(reviewId, userDetail);
     }
 }
